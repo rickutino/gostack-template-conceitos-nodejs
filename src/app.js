96449594bce1e5eb8a4fa15186c5647bb7e192ae
rickutino <rickutino@gmail.com>
2020-07-01 20:32:29 +0900

    init commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { } = request.body;
 });
 
 app.put("/repositories/:id", (request, response) => {
